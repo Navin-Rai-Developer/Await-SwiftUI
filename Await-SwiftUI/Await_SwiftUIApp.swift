acc388422871dad50bbeb01fa6c3c75c51559576
Navin Rai <rai.navinrai20@gmail.com>
2025-05-29 22:31:38 +0530

    Initial Commit

---
+++ Await-SwiftUI/Await_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Await_SwiftUIApp.swift
+//  Await-SwiftUI
+//
+//  Created by Navin Rai on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Await_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
