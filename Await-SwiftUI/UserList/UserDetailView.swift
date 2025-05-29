c64f4892e9c556e449cd257f89983b66b1a0cda7
Navin Rai <rai.navinrai20@gmail.com>
2025-05-29 23:48:39 +0530

    "Added UserDetailView and connected navigation"

---
+++ Await-SwiftUI/UserList/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  Await-SwiftUI
+//
+//  Created by Navin Rai on 29/05/25.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
