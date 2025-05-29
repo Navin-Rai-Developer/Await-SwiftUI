ff5fb646aebbc543aa6b18019721617081f71278
Navin Rai <rai.navinrai20@gmail.com>
2025-05-29 23:58:11 +0530

    Added UserDetailView and updated UserListView with navigation

--- Await-SwiftUI/UserList/UserDetailView.swift
+++ Await-SwiftUI/UserList/UserDetailView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct UserDetailView: View {
+    let user: UserModel  // Replace `UserModel` with your actual model name
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            AsyncImage(url: URL(string: user.avatarURL ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.teal)
+            }
+            .frame(width: 120, height: 120)
+
+            Text(user.login ?? "Unknown User")
+                .font(.title)
+                .bold()
+
+            Text(user.url ?? "No URL")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("User Details")
     }
 }
 
 #Preview {
-    UserDetailView()
+    UserDetailView(user: UserModel(
+        id: 1,
+        login: "navinrai",
+        nodeID: "MDQ6VXNlcjE=",
+        avatarURL: "https://avatars.githubusercontent.com/u/1?v=4",
+        gravatarID: "",
+        url: "https://api.github.com/users/navinrai",
+        htmlURL: "https://github.com/navinrai",
+        followersURL: "https://api.github.com/users/navinrai/followers",
+        followingURL: "https://api.github.com/users/navinrai/following{/other_user}",
+        gistsURL: "https://api.github.com/users/navinrai/gists{/gist_id}",
+        starredURL: "https://api.github.com/users/navinrai/starred{/owner}{/repo}",
+        subscriptionsURL: "https://api.github.com/users/navinrai/subscriptions",
+        organizationsURL: "https://api.github.com/users/navinrai/orgs",
+        reposURL: "https://api.github.com/users/navinrai/repos",
+        eventsURL: "https://api.github.com/users/navinrai/events{/privacy}",
+        receivedEventsURL: "https://api.github.com/users/navinrai/received_events",
+        type: "User",
+        siteAdmin: false
+    ))
 }
--- Await-SwiftUI/UserList/UserListView.swift
+++ Await-SwiftUI/UserList/UserListView.swift
@@ -16,36 +16,38 @@ struct UserListView: View {
             ZStack {
                 Color.white.ignoresSafeArea()
                 
-                List(viewModel.users ?? [], id: \.id) {
-                    user in
-                    HStack {
-                        AsyncImage(url: URL(string: user.avatarURL ?? "")) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .clipShape(Circle())
-                        } placeholder: {
-                            Circle()
-                                .foregroundColor(.teal)
-                        }
-                        .frame(width: 50, height: 50)
-                        
-                        VStack(alignment: .leading) {
-                            Text(user.login?.capitalized ?? "")
-                                .font(.headline)
-                            Text(user.url ?? "")
-                                .font(.subheadline)
+                List(viewModel.users ?? [], id: \.id) { user in
+                    NavigationLink(destination: UserDetailView(user: user)) {
+                        HStack {
+                            AsyncImage(url: URL(string: user.avatarURL ?? "")) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipShape(Circle())
+                            } placeholder: {
+                                Circle()
+                                    .foregroundColor(.teal)
+                            }
+                            .frame(width: 50, height: 50)
+                            
+                            VStack(alignment: .leading) {
+                                Text(user.login?.capitalized ?? "")
+                                    .font(.headline)
+                                Text(user.url ?? "")
+                                    .font(.subheadline)
+                            }
                         }
                     }
                 }
                 .listStyle(.plain)
                 .listRowInsets(EdgeInsets())
                 .background(Color.white)
-                .navigationTitle("Users")
+                
                 if viewModel.isLoading {
                     LoaderView()
                 }
             }
+            .navigationTitle("Users")
         }
         .task {
             await viewModel.getUser()
